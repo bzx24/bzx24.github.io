a0929fcf09b3e452c4fb87aa244eba41c41cf25c
bzx24 <brianxu16@gmail.com>
2021-07-04 23:02:03 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/navbar/Navbar"
+import Landing from "./components/landing/Landing"
+import About from "./components/about/About"
+import Projects from "./components/projects/Projects"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      {/* <div className="sections">
+        <Landing/>
+        <About/>
+        <Projects/>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
