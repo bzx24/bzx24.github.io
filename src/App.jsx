c78bab9e2c4e26ae16782acddd98573f0a3eca70
bzx24 <brianxu16@gmail.com>
2021-07-31 16:32:33 -0500

    finished home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import Navbar from "./components/navbar/Navbar"
-import Landing from "./components/landing/Landing"
-import About from "./components/about/About"
-import Projects from "./components/projects/Projects"
+import Navbar from "./components/navbar/Navbar";
+import Landing from "./components/landing/Landing";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      {/* <div className="sections">
-        <Landing/>
-        <About/>
-        <Projects/>
-      </div> */}
+      <Landing/>
     </div>
   );
 }
