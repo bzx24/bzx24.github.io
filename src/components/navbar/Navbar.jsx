ab0054c009e73f7c52c485733aa4e69a22191be9
bzx24 <brianxu16@gmail.com>
2021-07-31 16:33:02 -0500

    added buttons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import "./navbar.scss"
+import Button from '@material-ui/core/Button';
+import { init } from "ityped";
+import "./navbar.scss";
 
 export default function Navbar() {
     return (
         <div className = "navbar">
-            
+            <div className = "wrapper">
+                <div className = "left">
+                    <div className = "button">
+                        <Button style = {{color: "#FAFAFA", fontSize: "15px", font: "Roboto"}} variant="text" size="small">Home</Button>
+                        <Button style = {{color: "#FAFAFA", fontSize: "15px", font: "Roboto"}} variant="text" size="small">About</Button>
+                        <Button style = {{color: "#FAFAFA", fontSize: "15px", font: "Roboto"}} variant="text" size="small">Projects</Button>
+                        <Button style = {{color: "#FAFAFA", fontSize: "15px", font: "Roboto"}} variant="text" size="small">Blog</Button>
+                    </div>
+                </div>
+                <div className = "right">
+                    <div className = "icons-social">
+                        <a target="_blank" href="https://github.com/bzx24"><i class="fab fa-github"></i></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/bzxu"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                    <div className = "button">
+                        <Button style = {{color: "#FAFAFA", backgroundColor: "#21b6ae", fontSize: "15px", font: "Roboto"}} variant="contained" size="small">RESUME</Button>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
